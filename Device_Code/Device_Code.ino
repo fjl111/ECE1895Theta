c8086c2cea139f5e004e57e5e4c7b14bf5d8c13b
lesliemcdonough <lpm43@pitt.edu>
2024-10-28 10:38:51 -0400

    Update Device_Code.ino

--- Device_Code/Device_Code.ino
+++ Device_Code/Device_Code.ino
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SD.h> 
+#include <TMRpcm.h>
+#include <SPI.h>
 
 //Create the global variables
 int score, time, playing;
@@ -8,6 +11,8 @@ int score, time, playing;
 #define SOLDER_PIN 4
 #define CODE_PIN 5
 #define MULTIMETER_PIN 23
+#define SD_ChipSelectPin 10
+TMRcpm tmrcpm;
 
 typedef enum {
     SOLDER_IT,
@@ -17,7 +22,6 @@ typedef enum {
 
 void setup() {
   //Connect to the serial monitor
-  Serial.begin(9600);
 
   Serial.println("Startup");
 
@@ -31,6 +35,15 @@ void setup() {
   pinMode(CODE_PIN, INPUT);
   pinMode(MULTIMETER_PIN, INPUT);
 
+   // Initialize SD card
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+  Serial.println("SD card is ready to use.");
+
+  // Setup TMRpcm
+  tmrpcm.speakerPin = 9;
 }
 
 void loop() {
@@ -47,20 +60,32 @@ void loop() {
       //If the solder pin is high, the solder action has been completed
       if(digitalRead(SOLDER_PIN) == HIGH){
         Serial.println("Soldering is being done");
+        playSound("solder.wav");
+        break;
       }
 
       //If the code pin is high, the code action has been completed
       if(digitalRead(CODE_PIN) == HIGH){
         Serial.println("Coding is being done");
+        playSound("code.wav");
+        break;
       }
 
       //If the anologue pin is not close to 0 (being pulled down), the multimeter action is being complete
       if(analogRead(MULTIMETER_PIN) > 10){
         //Check to make sure the correct resistor is being probed (the analogue input is in the right range)
         Serial.println(analogRead(MULTIMETER_PIN));
+        playSound("meter.wav");
+        break;
       }
     }
-    
   }
 
+  void playSound(const char* fileName){
+    if(tmrpcm.isPlaying()){
+      //stops dound, keeps timer running
+      tmrpcm.stopPlayback(); 
+    }
+    tmrpcm.play(fileName);
+  }
 }
