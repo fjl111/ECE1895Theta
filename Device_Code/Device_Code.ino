6f06a832fa032653dc341a0c776cfedd9928d395
fjl111 <115193218+fjl111@users.noreply.github.com>
2024-10-30 19:45:54 -0400

    Code Updated

--- Device_Code/Device_Code.ino
+++ Device_Code/Device_Code.ino
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-#include <DFRobot_DF1201S.h>
+#include <DFRobotDFPlayerMini.h>
 
 typedef enum {
     SOLDER_IT,
@@ -15,17 +15,19 @@ typedef enum {
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 //Set up SD mp3 player
-SoftwareSerial DF1201SSerial(15, 16); //RX TX
+SoftwareSerial mySoftwareSerial(10, 11); //RX TX
+DFRobotDFPlayerMini myDFPlayer;
 
 //Define the pins
-#define SOLDER_PIN 4
-#define CODE_PIN 5
-#define MULTIMETER_PIN 23
-#define ON_BUTTON 6
+#define SOLDER_PIN 2
+#define CODE_PIN 3
+#define MULTIMETER_PIN A0
+#define ON_BUTTON 4
 
 //Create the global variables
 int score, time, playing;
-bool gameOver;
+int starttime, endtime;
+bool gameOver, correctChoice;
 COMMAND currentTask;
 
 void setup() {
@@ -36,38 +38,26 @@ void setup() {
   delay(2000);
   
   //Connect to the serial monitor
-  Serial.begin(9600);
-
   Serial.begin(115200);
-  DF120SSerial.begin(115200);
-
-  while(!DF1201S.begin(DF1201SSerial)){
-    Serial.println("Init failed, please check the wire connection!");
-    delay(1000);
-  }
-
-  //set volume
-  DF1201S.setVol(/*VOL = */20);
-  Serial.print("VOL:");
-
-  //get volume
-  Serial.println(DF1201S.getVol());
-
-  //switch function to play music
-  DF1201S.switchFunction(DF1201S.MUSIC);
-  delay(2000);
-
-  DF1201S.setPlayMode(DF1201S.ALLCYCLE);
-  Serial.print("PlayMode:");
-
-  //get playback mode
-  Serial.println(DF1201S.getPlayMode());
-
   Serial.println("Startup");
 
+  //Connect to the DFPlayer
+  mySoftwareSerial.begin(9600);
+
+  //Start the MP3 player
+  Serial.println("Initializing DFPlayer ... (May take 3~5 seconds)");
+  while (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    delay(100);
+  }
+  Serial.println("DFPlayer Mini online.");
+
+  //Set the DFPlayer Volume (range is 0 to 30)
+  myDFPlayer.volume(10);
+
   //Set the global variables
-  score = 0;
-  time = 0;
   playing = false;
   gameOver = false;
 
@@ -90,84 +80,107 @@ void displaynumber(int num){
 
 void loop() {
 
-  displaynumber(99);
   //Play the file No.1, the numbers are arranged according to the sequence of the files copied into the U-disk 
-  DF1201S.playFileNum(/*File Number = */1);
+  //Serial.println("Playing song");
   
-  // //If the on button is pressed, turn the playing variable on so that the game starts and reset gameOver
-  // if(digitalRead(ON_BUTTON) == HIGH){
-  //   gameOver = false;
-  //   playing = true;
-  // }
+  //If the on button is pressed, turn the playing variable on so that the game starts and reset gameOver
+  if(digitalRead(ON_BUTTON) == HIGH){
+    gameOver = false;
+    playing = true;
+    timeLimit = 12000;
+    score = 0;
+  }
 
-  // //Loop through the main code if we are in the play condition
-  // if(playing){
-  //   while(!gameOver){
-  //     //We will generate a random command here
-  //     currentTask = SOLDER_IT;
+  //Loop through the main code if we are in the play condition
+  if(playing){
+    displaynumber(score);
+    myDFPlayer.playMp3Folder(/*File Number = */1);
+    delay(5000);
+    Serial.println("Game Started");
+    while(!gameOver){
+      //We will generate a random command here
+      currentTask = MULTIMETER_IT;
+      Serial.println("Issued Command");
+      displaynumber(score);
 
-  //     //Check the input while the time is counting down
-  //     while(time > 0){
-  //       //Once we receive a command check if it matches the randomly generated command
-  //       //If it does, break out of the loop and continue. If it doesn't break out of the loop and end the game
+      //Initialize the times for the countdown
+      starttime = millis();
+      endtime = starttime;
+      //Check the input while the time is counting down
+      while((starttime-endtime) < time_limit){
+        //Once we receive a command check if it matches the randomly generated command
+        //If it does, break out of the loop and continue. If it doesn't break out of the loop and end the game
+        Serial.println("Waiting for input");
+        //If the solder pin is high, the solder action has been completed
+        if(digitalRead(SOLDER_PIN) == HIGH){
+          Serial.println("Soldering is being done");
+          //If this is the correct action, increment the score
+          if(currentTask == SOLDER_IT){
+            Serial.println("Correct Action");
+            correctChoice = true;
+          //Otherwise, set gameOver to true
+          }else{
+            gameOver = true;
+          }
+          break;
+        }
 
-  //       //If the solder pin is high, the solder action has been completed
-  //       if(digitalRead(SOLDER_PIN) == HIGH){
-  //         Serial.println("Soldering is being done");
-  //         //If this is the correct action, increment the score
-  //         if(currentTask == SOLDER_IT){
-  //           Serial.println("Correct Action");
-  //           score++;
-  //         //Otherwise, set gameOver to true
-  //         }else{
-  //           gameOver = true;
-  //         }
-  //         break;
-  //       }
+        //If the code pin is high, the code action has been completed
+        if(digitalRead(CODE_PIN) == HIGH){
+          Serial.println("Coding is being done");
+          //If this is the correct action, increment the score
+          if(currentTask == CODE_IT){
+            Serial.println("Correct Action");
+            correctChoice = true;
+          //Otherwise, set gameOver to true
+          }else{
+            gameOver = true;
+          }
+          //Exit the loop waiting for an action since an action was complete
+          break;
+        }
 
-  //       //If the code pin is high, the code action has been completed
-  //       if(digitalRead(CODE_PIN) == HIGH){
-  //         Serial.println("Coding is being done");
-  //         //If this is the correct action, increment the score
-  //         if(currentTask == CODE_IT){
-  //           Serial.println("Correct Action");
-  //           score++;
-  //         //Otherwise, set gameOver to true
-  //         }else{
-  //           gameOver = true;
-  //         }
-  //         //Exit the loop waiting for an action since an action was complete
-  //         break;
-  //       }
+        //If the anologue pin is not close to 0 (being pulled down), the multimeter action is being complete
+        if(analogRead(MULTIMETER_PIN) > 10){
+          //If this is the correct action, check to make sure the correct resistor is being probed (the analogue input is in the right range)
+          if(currentTask == MULTIMETER_IT){
+            Serial.println("Correct Action");
+            int resistorVal = analogRead(MULTIMETER_PIN);
+            Serial.println(resistorVal);
+            //If the correct resistor is being probed, increment the score
+            if(resistorVal > 500 && resistorVal < 550){
+              correctChoice = true;
+            //Otherwise, set game over to true
+            }else{
+              gameOver = true;
+            }
+          //Otherwise, set gameOver to true
+          }else{
+            gameOver = true;
+          }
+          //Exit the loop waiting for an action since an action was complete
+          break;
+        }
+      }
 
-  //       //If the anologue pin is not close to 0 (being pulled down), the multimeter action is being complete
-  //       if(analogRead(MULTIMETER_PIN) > 10){
-  //         //If this is the correct action, check to make sure the correct resistor is being probed (the analogue input is in the right range)
-  //         if(currentTask == MULTIMETER_IT){
-  //           Serial.println("Correct Action");
-  //           int resistorVal = analogRead(MULTIMETER_PIN);
-  //           Serial.println(resistorVal);
-  //           //If the correct resistor is being probed, increment the score
-  //           if(resistorVal > 0 && resistorVal < 1023){
-  //             score++;
-  //           //Otherwise, set game over to true
-  //           }else{
-  //             gameOver = true;
-  //           }
-  //         //Otherwise, set gameOver to true
-  //         }else{
-  //           gameOver = true;
-  //         }
-  //         //Exit the loop waiting for an action since an action was complete
-  //         break;
-  //       }
-  //     }
-      
-  //   }
-  // }
+      //Check to see if the correct selection was made
+      if(correctChoice){
+        //If the user made the correct choice, increment the score and decrease the available time by 0.1s
+        score++;
+        timeLimit -= 100;
+      }else{
+        //If the user ran out of time, set gameOver to true
+        gameOver = true;
+      }
+    }
 
-  //If the game already ran and we now are at gameOver, play the end message
-  if(gameOver){
+    //If the game already ran and we now are at gameOver, play the end message
+    if(gameOver){
+
+      //Set playing to false
+      playing = false;
+    }
 
   }
+
 }
